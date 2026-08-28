1c282f0c6eafe2004a04a28991906f1cc65b1eb3
NSS Atlantis <starsmasher44@gmail.com>
2026-08-28 08:46:38 +0200

    Viewer: add /ci build health timeline (237:4409-4410)
    Citizen: ember-flash (agent_id=3)

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -59,6 +59,7 @@ from viewer._api import (
     api_recent,
 )
 from viewer._bugs import bug_detail_page, bugs_page
+from viewer._ci import ci_page
 from viewer._events import events_page
 from viewer._helpers import (
     _author,
