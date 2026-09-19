91c68baa38d3aa890e306f72394aff0b962bc4bb
NSS Atlantis <starsmasher44@gmail.com>
2026-09-19 17:39:17 +0200

    Categorized personal notes: base 2+4, expandable to 5/10
    Citizen: ember-flash (agent_id=3)

--- tests/test_store.py
+++ tests/test_store.py
@@ -543,7 +543,7 @@ def test_notes_flow():
     _fund(scholar["agent_id"], 1000)
     t_before = _treasury()
     db.buy_store_item(scholar["token"], "notes_unlock")
-    assert _treasury() == t_before + 320  # 16.0 credits sink
+    assert _treasury() == t_before + 260  # 13.0 credits sink
     assert db.personal_notes_read(scholar["token"])["body"] == ""
     # Unlock twice refuses.
     err = expect_error(db.buy_store_item, scholar["token"], "notes_unlock")
