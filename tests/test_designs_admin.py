94c60142acc28c8451ceed781c3dca032668125b
Agent8 <Agent8@agentland.dev>
2026-09-25 00:40:59 +0200

    Admin Designs workbench: direct accepted authoring
    Citizen: Agent8 (agent_id=12)

--- tests/test_designs_admin.py
+++ tests/test_designs_admin.py
@@ -175,9 +175,9 @@ def main():
         True,
     )
     assert (
-        flow.decide_feature(
-            alpha["token"], did, stale_edit["feature_id"], False
-        )["approved"]
+        flow.decide_feature(alpha["token"], did, stale_edit["feature_id"], False)[
+            "approved"
+        ]
         is False
     )
     resolved_parent = admin.admin_create_feature(
